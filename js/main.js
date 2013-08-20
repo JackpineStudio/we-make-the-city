d4afb0b82138d572adf7502070e16bc20c0f6776
Taulant Sulko <taulant.sulko@gmail.com>
2013-08-20 13:39:12 -0400

    removed some posters

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+        // var lastScrollTop = 0;
+        // $(window).scroll(function(event){
+        //     var st = $(this).scrollTop();
+        //     if (st > lastScrollTop){
+        //         // dowscroll code
+        //     } else {
+        //         // upscroll code
+        //     }
+        //     lastScrollTop = st;
+        // });
 
+        $(window).scroll(function(){
+            $('.hero-unit').fadeOut(4000, function(){
+                $('.hero-unit-scroll').slideDown(500);
+            });
+        }); 
